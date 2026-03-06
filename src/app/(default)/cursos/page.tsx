d732ee7b2e126b31a6b0670a89726eb996749953
Diego Pongeti <diego-p43@hotmail.com>
2026-03-05 23:12:35 -0300

    add youtube service

--- src/app/(default)/cursos/page.tsx
+++ src/app/(default)/cursos/page.tsx
@@ -2,12 +2,15 @@
 import { Metadata } from "next";
 import { Section } from "../../components/section/Section";
 
+import { APIYouTube } from "@//shared/services/api-youtube";
+
 export const metadata: Metadata = {
     title: "Codarse - Todos os Cursos"
 }
 
 
-export default function PageCursos() {
+export default async function PageCursos() {
+    const courses = await APIYouTube.course.getAll();
     return (
         <main className="mt-8 flex justify-center ">
             <div className="w-full min-[880px]:max-w-[880px]">
@@ -15,145 +18,14 @@ export default function PageCursos() {
                 <Section
                     title="Todos os Cursos"
                     variant="grid"
-                    items={[
-                        {
-                            href: "/cursos/123",
-                            image: "https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg",
-                            title: "Curso de API Rest, Node e TypeScript",
-                            description: `No processo de desenvolvimento, as vezes precisamos criar componentes e outros recursos que nos farão performar muito melhor no futuro. A criação de bons componentes de interface é o segredo para um desenvolvimento rápido de uma aplicação frontend.
-                                    Nessa vamos criar junto o componente de card da nossa aplicação, esse componente será usado em toda a nossa plataforma de cursos, e tem as melhores práticas de desenvolvimento aplicadas.
-
-
-                                    Links mencionados no vídeo:
-                                    📖 Repositório do projeto - https://github.com/lvsouza/youtube-ne...
-                                    🅵 Protótipo - https://www.figma.com/community/file/...
-                                    🗨️ Discord -   / discord
-
-                                    Outros cursos no canal:
-                                    🔔 NextJS, TailwindCSS e Typescript -    • NextJS, TailwindCSS e Typescript: #00 - Ap...
-                                    🎩 Figma para DEVs -    • Figma para DEVs: #00 - Apresentação do curso
-                                    ☪️ Curso de estilização no React -    • Estilização no React: Aula 00 - Curso de f...
-                                    ⭐️ Curso de React com typescript -    • Curso de react: Aula 01 - Começando com Re...
-                                    👑 React, Material UI 5 e Typescript -    • React, Material UI 5 e Typescript: #00 - I...
-                                    🏆 Curso de API Rest, Node e Typescript -    • API Rest, Node e Typescript: #00 - Apresen...
-
-                                    Livros recomendados:
-                                    📘 Código limpo - https://amzn.to/43Xiick
-                                    📘 Arquitetura limpa - https://amzn.to/3ZMCStr
-                                    📘 Migrando sistemas monolíticos - https://amzn.to/45ByPDZ
-                                    📘 The Micro SaaS Handbook - https://amzn.to/4jCan93
-
-                                    Conteúdo:
-                                    0:00 | Introdução
-                                    0:44 | O que faremos nessa aula
-                                    2:33 | Iniciando a construção do componente de Card
-                                    10:00 | Como utilizar as APIs do YouTube
-                                    19:00 | Deixando imagem não arrastavel
-                                    19:30 | Ajustando o CSS do card
-                                    27:00 | Adicionando link no card
-                                    30:00 | Configurando card para modo desktop
-                                    32:30 | Commit dos ajustes
-                                    33:00 | Finalização e agradecimentos
-
-
-                                    CODARSE - Tela inicial, de cursos e components
-
-                                    #DesenvolvimentoWeb #Programação #DesenvolvimentoDeSoftware #AprenderProgramação #CursosOnline #TechTutoriais #YouTubeEducação #CodeNewbies #DesenvolvimentoFrontend #CodeWithMe #TecnologiaEducacional #WebDesign #DesenvolvimentoDeAplicativos #AprendaACodificar #TecnologiaDeFrontend #EducaçãoDigital #DesenvolvimentoDeApps #TutoriaisDeProgramação #CódigoCriativo #ProgramaçãoParaIniciantes #NextJS #typescript #javascript #tailwindcss #react #reactjs`
-
-                        },
-
-                        {
-                            href: "/cursos/123",
-                            image: "https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg",
-                            title: "Curso de API Rest, Node e TypeScript",
-                            description: `No processo de desenvolvimento, as vezes precisamos criar componentes e outros recursos que nos farão performar muito melhor no futuro. A criação de bons componentes de interface é o segredo para um desenvolvimento rápido de uma aplicação frontend.
-                                    Nessa vamos criar junto o componente de card da nossa aplicação, esse componente será usado em toda a nossa plataforma de cursos, e tem as melhores práticas de desenvolvimento aplicadas.
-
-
-                                    Links mencionados no vídeo:
-                                    📖 Repositório do projeto - https://github.com/lvsouza/youtube-ne...
-                                    🅵 Protótipo - https://www.figma.com/community/file/...
-                                    🗨️ Discord -   / discord
-
-                                    Outros cursos no canal:
-                                    🔔 NextJS, TailwindCSS e Typescript -    • NextJS, TailwindCSS e Typescript: #00 - Ap...
-                                    🎩 Figma para DEVs -    • Figma para DEVs: #00 - Apresentação do curso
-                                    ☪️ Curso de estilização no React -    • Estilização no React: Aula 00 - Curso de f...
-                                    ⭐️ Curso de React com typescript -    • Curso de react: Aula 01 - Começando com Re...
-                                    👑 React, Material UI 5 e Typescript -    • React, Material UI 5 e Typescript: #00 - I...
-                                    🏆 Curso de API Rest, Node e Typescript -    • API Rest, Node e Typescript: #00 - Apresen...
-
-                                    Livros recomendados:
-                                    📘 Código limpo - https://amzn.to/43Xiick
-                                    📘 Arquitetura limpa - https://amzn.to/3ZMCStr
-                                    📘 Migrando sistemas monolíticos - https://amzn.to/45ByPDZ
-                                    📘 The Micro SaaS Handbook - https://amzn.to/4jCan93
-
-                                    Conteúdo:
-                                    0:00 | Introdução
-                                    0:44 | O que faremos nessa aula
-                                    2:33 | Iniciando a construção do componente de Card
-                                    10:00 | Como utilizar as APIs do YouTube
-                                    19:00 | Deixando imagem não arrastavel
-                                    19:30 | Ajustando o CSS do card
-                                    27:00 | Adicionando link no card
-                                    30:00 | Configurando card para modo desktop
-                                    32:30 | Commit dos ajustes
-                                    33:00 | Finalização e agradecimentos
-
-
-                                    CODARSE - Tela inicial, de cursos e components
-
-                                    #DesenvolvimentoWeb #Programação #DesenvolvimentoDeSoftware #AprenderProgramação #CursosOnline #TechTutoriais #YouTubeEducação #CodeNewbies #DesenvolvimentoFrontend #CodeWithMe #TecnologiaEducacional #WebDesign #DesenvolvimentoDeAplicativos #AprendaACodificar #TecnologiaDeFrontend #EducaçãoDigital #DesenvolvimentoDeApps #TutoriaisDeProgramação #CódigoCriativo #ProgramaçãoParaIniciantes #NextJS #typescript #javascript #tailwindcss #react #reactjs`
-
-                        },
-
-                        {
-                            href: "/cursos/123",
-                            image: "https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg",
-                            title: "Curso de API Rest, Node e TypeScript",
-                            description: `No processo de desenvolvimento, as vezes precisamos criar componentes e outros recursos que nos farão performar muito melhor no futuro. A criação de bons componentes de interface é o segredo para um desenvolvimento rápido de uma aplicação frontend.
-                                    Nessa vamos criar junto o componente de card da nossa aplicação, esse componente será usado em toda a nossa plataforma de cursos, e tem as melhores práticas de desenvolvimento aplicadas.
-
-
-                                    Links mencionados no vídeo:
-                                    📖 Repositório do projeto - https://github.com/lvsouza/youtube-ne...
-                                    🅵 Protótipo - https://www.figma.com/community/file/...
-                                    🗨️ Discord -   / discord
-
-                                    Outros cursos no canal:
-                                    🔔 NextJS, TailwindCSS e Typescript -    • NextJS, TailwindCSS e Typescript: #00 - Ap...
-                                    🎩 Figma para DEVs -    • Figma para DEVs: #00 - Apresentação do curso
-                                    ☪️ Curso de estilização no React -    • Estilização no React: Aula 00 - Curso de f...
-                                    ⭐️ Curso de React com typescript -    • Curso de react: Aula 01 - Começando com Re...
-                                    👑 React, Material UI 5 e Typescript -    • React, Material UI 5 e Typescript: #00 - I...
-                                    🏆 Curso de API Rest, Node e Typescript -    • API Rest, Node e Typescript: #00 - Apresen...
-
-                                    Livros recomendados:
-                                    📘 Código limpo - https://amzn.to/43Xiick
-                                    📘 Arquitetura limpa - https://amzn.to/3ZMCStr
-                                    📘 Migrando sistemas monolíticos - https://amzn.to/45ByPDZ
-                                    📘 The Micro SaaS Handbook - https://amzn.to/4jCan93
-
-                                    Conteúdo:
-                                    0:00 | Introdução
-                                    0:44 | O que faremos nessa aula
-                                    2:33 | Iniciando a construção do componente de Card
-                                    10:00 | Como utilizar as APIs do YouTube
-                                    19:00 | Deixando imagem não arrastavel
-                                    19:30 | Ajustando o CSS do card
-                                    27:00 | Adicionando link no card
-                                    30:00 | Configurando card para modo desktop
-                                    32:30 | Commit dos ajustes
-                                    33:00 | Finalização e agradecimentos
-
-
-                                    CODARSE - Tela inicial, de cursos e components
-
-                                    #DesenvolvimentoWeb #Programação #DesenvolvimentoDeSoftware #AprenderProgramação #CursosOnline #TechTutoriais #YouTubeEducação #CodeNewbies #DesenvolvimentoFrontend #CodeWithMe #TecnologiaEducacional #WebDesign #DesenvolvimentoDeAplicativos #AprendaACodificar #TecnologiaDeFrontend #EducaçãoDigital #DesenvolvimentoDeApps #TutoriaisDeProgramação #CódigoCriativo #ProgramaçãoParaIniciantes #NextJS #typescript #javascript #tailwindcss #react #reactjs`
-
-                        }
-                    ]}
+                    items={
+                        courses.map(course => ({
+                            title: course.title,
+                            image: course.image,
+                            href: `/cursos/${course.id}`,
+                            description: course.description
+                        }))
+                    }
                 />
             </div>
         </main>
---
+++ src/shared/services/api-youtube/index.ts
@@ -0,0 +1,28 @@
+import { youtube } from "@googleapis/youtube";
+
+
+const YouTubeAPIClient = youtube({
+    version: 'v3',
+    auth: process.env.YOUTUBE_API_KEY,
+})
+
+export const APIYouTube = {
+    course: {
+        getAll: async () => {
+            const { data } = await YouTubeAPIClient.playlists.list({
+                maxResults: 50,
+                part: ['snippet'],
+                channelId: 'UCtJ3j1ANBe91ZHXzw6FJveg'
+            })
+
+            const courses = (data.items || []).map(item => ({
+                id: item.id || '',
+                title: item.snippet?.title || '',
+                description: item.snippet?.description || '',
+                image: item.snippet?.thumbnails?.medium?.url || '',
+            }));
+
+            return courses.filter(course => course.description.includes('#CORDARSE'))
+        }
+    }
+}
